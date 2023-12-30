22a368152788fe867dc17cbe6494506e49ff9484
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-12-30 13:41:23 +0100

    fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
-from flask import Flask
+"""
+A script that starts a Flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
@@ -16,4 +18,3 @@ def hbnb():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
-
