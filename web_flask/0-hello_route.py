8ddb0128cbaf6e34adb86e3af11c096d4e405f28
Sliman Elmansouri <elmansourisliman@gmail.com>
2024-01-01 13:00:52 +0100

    readme

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-a script that starts a Flask web application:
-
-The application must be listening on 0.0.0.0, port 5000
-Routes:
-/: display “Hello HBNB!”
-It must use the option strict_slashes=False in your route definition
-
-"""
+"""A script that starts a web application with Flask."""
 
 from flask import Flask
 
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application:
-
-The application must be listening on 0.0.0.0, port 5000
-You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
-After each request you must remove the current SQLAlchemy Session:
-Declare a method to handle @app.teardown_appcontext
-Call in this method storage.close()
-Routes:
-/states_list: display a HTML page: (inside the tag BODY)
-H1 tag: “States”
-UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
-LI tag: description of one State: <state.id>: <B><state.name></B>
-Import this 7-dump to have some data
-You must use the option strict_slashes=False in your route definition
-
-"""
+"""A script that starts a Flask web app."""
 
 from flask import Flask, render_template
-from models import *
+# from models import *
 from models import storage
 
 app = Flask(__name__)
