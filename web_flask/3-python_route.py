8a452451df62dd9ea10868a24c24310ee706d02e
Sliman Elmansouri <elmansourisliman@gmail.com>
2024-01-01 13:17:03 +0100

    pycodestyle fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 """
-A script that starts a Flask web application:
-
-The web application must be listening on 0.0.0.0, port 5000
-Routes:
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-/python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-The default value of text is “is cool”
-It must use the option strict_slashes=False in your route definition
-
+A script that starts a Flask web application.
 """
 
 from flask import Flask
